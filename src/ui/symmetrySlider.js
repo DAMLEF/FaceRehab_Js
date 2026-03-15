8c4045f2780daf3a792d5d672b6a25a4c7418d11
Damien LEFEUVRE <damien.lefeuvre2@laposte.net>
2026-03-15 12:56:06 +0100

    Ajout des profils de symétrie

--- src/ui/symmetrySlider.js
+++ src/ui/symmetrySlider.js
@@ -53,6 +53,48 @@ function createSymSliderControl(name){
     return button;
 }
 
+// Téléchargement du profil symétrique de l'utilisateur
+function downloadSymProfile(){
+    // On définit nos données JSON pour le profil
+    const profile = JSON.stringify(allSymSlidersValues, null, 2);
+
+    // On crée l'objet BLOB
+    const blobObj = new Blob([profile], { type: "application/json" });
+    const urlProfile = URL.createObjectURL(blobObj);
+
+    const aDownload = document.createElement('a');
+
+    const now = new Date();
+    const fileName = `symProfile_${now.getTime()}.json`;
+
+    aDownload.setAttribute('href', urlProfile);
+    aDownload.setAttribute('download', fileName);
+
+    aDownload.click();
+
+    URL.revokeObjectURL(urlProfile);
+
+}
+
+function loadSymProfile(){
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", ".json");
+
+    input.onchange = async () => {
+
+        const file = input.files[0];
+        const fileText = await file.text()
+        const profileData = JSON.parse(fileText);
+
+        setSymSlidersProfile(profileData);
+    }
+
+    input.click();
+
+}
+
 // Initialisation des sliders à partir des BS qui ont une symétrie
 function initSymSlider(){
     for(const baseBS in symmetricBlendshapes){
@@ -75,13 +117,27 @@ function initSymSlidersControls(){
     rightButton.addEventListener("click", () => {
         setSymSlidersToRight();
     })
+
+    const saveProfile = createSymSliderControl("SAVE PROFILE");
+    saveProfile.addEventListener("click", () => {
+        downloadSymProfile();
+    })
+
+    const loadProfile = createSymSliderControl("LOAD PROFILE");
+    loadProfile.addEventListener("click", () => {
+        loadSymProfile();
+    })
 }
 
-// Fonction utilitaire pour modifier rapidement tous les sliders
+// Fonction utilitaire pour modifier les sliders
+function setSymSliderValue(baseBS, value = 0){
+    allSymSlidersValues[baseBS] = value;
+    allSymSliders[baseBS].value = value.toString();
+}
+
 function setSymSlidersValue(value = 0){
     for (const baseBS in allSymSlidersValues){
-        allSymSlidersValues[baseBS] = value;
-        allSymSliders[baseBS].value = value.toString();
+        setSymSliderValue(baseBS, value);
     }
 }
 
@@ -95,6 +151,31 @@ export function setSymSlidersToRight(){
 
 export function setSymSlidersToDefault(){
     setSymSlidersValue(0);
+}
+
+
+// Fonction pour appliquer un profil aux sliders
+function setSymSlidersProfile(symProfile){
+
+    console.log("🚀 Lecture du profil : " + symProfile)
+
+    for(const key in symProfile){
+        if(key in allSymSlidersValues){
+
+            if(symProfile[key] > 1 || symProfile[key] < -1){
+                console.log(`⚠️ La clé ${key} du profil n'est pas associé à une valeur valide (${symProfile[key]})`)
+            }
+            else{
+                setSymSliderValue(key, symProfile[key]);
+            }
+
+
+        }
+        else{
+            console.log(`⚠️ La clé ${key} du profil n'est pas reconnu comme un blendshape valide.`)
+        }
+
+    }
 }
 
 // ----------------------------------------------------------------
